c011e74b732aa6f9a56fa9c2519ff186be9fea4f
Denis Wilton <35562940+DennisWilton@users.noreply.github.com>
2022-08-20 21:46:06 -0400

    Criando tela Home (WIP)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,20 @@
-import { signOut } from "firebase/auth";
-import { useCallback, useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { AuthContext } from "../../AuthProvider";
-import { auth } from "../../firebase/auth";
+import { signOut } from 'firebase/auth';
+import { ReactNode, useCallback, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../../AuthProvider';
+import { auth } from '../../firebase/auth';
+import { Container, Header } from './Home.styles';
+
+const DEFAULT_PICTURE_URL =
+  'https://www.riobranco.ac.leg.br/imagens/pngtreeavatariconprofileiconmemberloginvectorisolatedpngimage_1978396.jpg/image';
+
+function Row({ children }: { children?: ReactNode }) {
+  return (
+    <div className='row'>
+      <div className='row-content'>{children}</div>
+    </div>
+  );
+}
 
 export default function Home() {
   const user = useContext(AuthContext);
@@ -11,14 +23,32 @@ export default function Home() {
     signOut(auth);
   }, []);
 
-  //if (!user) {
-  //  return <Navigate to="/login" />;
-  //}
-
   return (
     <>
-      Você já está logado como <b>{user?.email}</b>.
-      <button onClick={handleLogout}>Logout</button>
+      <Container>
+        <Row>
+          <Header>
+            <div className='column-left'>
+              <div className='circle picture' onClick={handleLogout}>
+                <img
+                  src={user?.photoURL ?? DEFAULT_PICTURE_URL}
+                  referrerPolicy='no-referrer'
+                  alt='profile'
+                ></img>
+              </div>
+            </div>
+            <div className='column-right'>
+              <span>Olá,</span>
+              <span className='username'>
+                {user?.displayName ?? 'Zé Coteiro'}
+              </span>
+            </div>
+          </Header>
+        </Row>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
+      </Container>
     </>
   );
 }
